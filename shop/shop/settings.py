c64b7002fafb638fbcad22693ee693e5bb1bcf3a
Dexterlente <dexter.lente18@gmail.com>
2023-04-30 06:00:50 +0800

    D

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# lastseentimeout amout of the time should elapse before a user is considered "offline".
+USER_ONLINE_TIMEOUT = 10
